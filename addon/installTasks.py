72293930173a95ffc55b0e52a3a142890ea0d04d
Noelia Ruiz Martínez <nrm1977@gmail.com>
2015-06-07 15:29:31 +0200

    Added option to import settings when reinstalling, suggested by Joseph.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -1,6 +1,9 @@
 # -*- coding: UTF-8 -*-
 
 import addonHandler
+import os
+import shutil
+import globalVars
 import gui
 import wx
 
@@ -17,3 +20,14 @@ def onInstall():
 				wx.YES|wx.NO|wx.ICON_WARNING)==wx.YES:
 					addon.requestRemove()
 			break
+	if os.path.isfile(os.path.join(globalVars.appArgs.configPath, "clipContentsDesigner.ini")):
+		if gui.messageBox(
+			# Translators: the label of a message box dialog.
+			_("You seem to have previous settings saved for this add-on. Do you want to import them?"),
+			# Translators: the title of a message box dialog.
+			_("Import ClipContentsDesigner add-on settings"),
+			wx.YES|wx.NO|wx.ICON_WARNING)==wx.YES:
+				try:
+					shutil.copy(os.path.join(globalVars.appArgs.configPath, "clipContentsDesigner.ini"), os.path.join(os.path.dirname(__file__), "globalPlugins", "clipContentsDesigner"))
+				except:
+					pass
