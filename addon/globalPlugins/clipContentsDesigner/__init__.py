3a87a17c3ba40b5d88bc86aee15298a4f16a827b
adil s <ah.shaikh97@gmail.com>
2020-10-03 09:48:52 +0530

    removed unwanted imports

--- addon/globalPlugins/clipContentsDesigner/__init__.py
+++ addon/globalPlugins/clipContentsDesigner/__init__.py
@@ -22,12 +22,7 @@ from globalCommands import SCRCAT_TEXTREVIEW, SCRCAT_CONFIG
 from logHandler import log
 from .clipmanager import recentClips, ClipManagerDialog
 
-import gettext
-import os
 
-localedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'locale')
-translate = gettext.translation('handroll', localedir, fallback=True)
-_ = translate.gettext
 addonHandler.initTranslation()
 
 # Constants
