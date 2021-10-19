d2909e005670e9572354a970d8eec81c3bb5dd59
Noelia Ruiz Martínez <nrm1977@gmail.com>
2021-10-19 05:50:14 +0200

    Fix bug with NVDA+windows+c

--- addon/globalPlugins/clipContentsDesigner/__init__.py
+++ addon/globalPlugins/clipContentsDesigner/__init__.py
@@ -216,8 +216,10 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	@script(
 		# Translators: message presented in input mode.
-		description=_("""Retrieves the selected string or the text from the previously set start marker up to
-		and including the current position of the review cursor, and adds it to the clipboard."""),
+		description=_(
+			"Retrieves the selected string or the text from the previously set start marker "
+			"up to	and including the current position of the review cursor, and adds it to the clipboard."
+		),
 		gesture="kb:NVDA+windows+c"
 	)
 	def script_add(self, gesture):
