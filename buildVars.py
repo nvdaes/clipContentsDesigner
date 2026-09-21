6989cff43daf0af1297892f66b888cba34f54c0d
Noelia Ruiz Martínez <nrm1977@gmail.com>
2026-09-21 05:10:56 +0200

    Update metadata

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,8 @@ addon_info = AddonInfo(
 	addon_version="52.0.0",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
-	addon_changelog=_('* Restored ability to check the clipboard content.'),
+	addon_changelog=_("""* Restored ability to check the clipboard content.
+* Updates to translations."""),
 	# Author(s)
 	addon_author='Noelia <nrm1977@gmail.com>, Abdel <abdelkrim.bensaid@gmail.com>',
 	# URL for the add-on documentation support
@@ -37,7 +38,7 @@ addon_info = AddonInfo(
 	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
 	addon_minimumNVDAVersion='2026.1',
 	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
-	addon_lastTestedNVDAVersion='2026.2',
+	addon_lastTestedNVDAVersion='2026.3',
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
